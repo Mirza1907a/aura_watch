c830f164583d43066c6a9951fdde8d3a41eb143b
Mirza <mirza1907a@gmail.com>
2024-05-11 18:35:47 +0500

    Aura Watch Website

---
+++ js/script.js
@@ -0,0 +1,21 @@
+        // imgs java start from here 
+        var fullimgbox = document.getElementById("fullimgbox");
+
+        function openfullimg(pic) {
+            var fullimgbox = document.getElementById("fullimgbox");
+
+          fullimgbox.style.display = "flex";
+          fullimg.src = pic;
+        }
+        function closefullimg() {
+          fullimgbox.style.display = "none";
+        }
+// date and time 
+
+
+
+
+// visitor count
+
+
+
